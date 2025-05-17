1d8c6902f1961f345e595fe53fe210c336dcf2e7
don <donny77776@gmail.com>
2025-05-17 13:46:00 +0100

    working footstep audio
    also deleted unnecessary audio files

--- Assets/Scripts/Enemy Footsteps.cs
+++ Assets/Scripts/Enemy Footsteps.cs
@@ -1,84 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyFootsteps : MonoBehaviour
 {
-    public AudioClip Footsteps;
+    public AudioClip[] Footsteps;
     private AudioSource audioSource;
-    private float oldPosition;
+    public float delay = 0.7f; //can be changed to match enemy speed
+    float startingTime = 0f;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
-        ChangeClip(1);
-    }
+        //ChangeClip(1);
 
-    void LateUpdate()
-    {
-        oldPosition = transform.position.x;
-        oldPosition = transform.position.y;
+
+        startingTime = Time.time;
     }
 
 
+    
 
     // Update is called once per frame
     void Update()
     {
 
-        if (transform.position.x > oldPosition)
-        {
-            PlayFootsteps();
-        }
+        //audioSource.clip = Footsteps[num - 1];
 
-        if (transform.position.y > oldPosition)
-        {
-            PlayFootsteps();
-        }
 
-        //if (oldPosition == oldPosition)
-        //{
-        //    audioSource.Stop();
-        //}
-        //if (IsMoving())
+        if (Time.time > startingTime + delay) //plays footsteps after the delay time has passed
+        {
+            startingTime = Time.time;
+            PlayFootsteps();
+        }   
+
+        // plays audio if the enemy position moves
+        //if (transform.position.x > oldPosition)
         //{
         //    PlayFootsteps();
         //}
 
-
+        //if (transform.position.y > oldPosition)
+        //{
+        //    PlayFootsteps();
+        //}
     }
 
-    //private bool IsMoving()
-    //{
-    //    if (transform.position.x > oldPosition)
-    //    {
-    //        return true;
-        
-    //}
-
+   
     private void PlayFootsteps()
     {
-
-        audioSource.Play();
-        
-        //int clipIndex = Random.Range(0, Footsteps.Length);
-        //audioSource.PlayOneShot(Footsteps[clipIndex]); 
+        int randIndex = UnityEngine.Random.Range(0, Footsteps.Length); //creates a random number within the range of the number of audio clips
+        audioSource.PlayOneShot(Footsteps[randIndex]); //plays one of the clips of that number
 
     }
-    private void ChangeClip(int num)
-    {
 
-        //audioSource.clip = Footsteps[num-1];
 
-    }
-    private void StopFootsteps()
-    {
 
-        audioSource.Stop();
 
-        
 
-    }
+    //private void ChangeClip(int num)
+    //{
+
+    //    audioSource.clip = Footsteps[num-1];
+
+    //}
+    
 
 }
