4740fc70396311e05d48bc5f442bdce8efe390ee
Sam <crozsam2003@gmail.com>
2025-05-30 16:15:56 +0100

    fixed health to not god mode

--- Assets/Scripts/Player Health Controller.cs
+++ Assets/Scripts/Player Health Controller.cs
@@ -6,7 +6,7 @@ public class PlayerHealthController : MonoBehaviour
 
 {
     public int currentHealth;
-    private int maxHealth = 500;
+    private int maxHealth = 5;
 
 
     public GameObject canvasPrefab;   
